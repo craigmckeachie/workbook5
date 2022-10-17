3b724a4de800168ec684c2587437c9ad79776b8a
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-17 14:00:45 -0400

    find, filter demos

---
+++ demos/find.js
@@ -0,0 +1,24 @@
+let menu = [
+  { id: 1, item: "Tacos", category: "Meal", price: 12.29 },
+  { id: 2, item: "Burger", category: "Meal", price: 7.29 },
+  { id: 3, item: "Salad", category: "Meal", price: 8.29 },
+  { id: 4, item: "Ice tea", category: "Drink", price: 2.19 },
+  { id: 5, item: "Coke", category: "Drink", price: 2.29 },
+];
+
+let searchId = 3;
+
+// function searchForId(menuItem) {
+//   if (menuItem.id === searchId) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+function test(menuItem) {
+  return menuItem.id === searchId;
+}
+
+const matchingMenuItem = menu.find(test);
+console.log(matchingMenuItem);
