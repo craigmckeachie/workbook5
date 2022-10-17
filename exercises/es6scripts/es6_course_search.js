5a8e3218e298bbdf50ce4cf40d661d8450ce06c3
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-17 14:49:14 -0400

    Exercise 1

---
+++ exercises/es6scripts/es6_course_search.js
@@ -0,0 +1,59 @@
+let courses = [
+  {
+    CourseId: "PROG100",
+    Title: "Introduction to HTML/CSS/Git",
+    Location: "Classroom 7",
+    StartDate: "09/08/22",
+    Fee: "100.00",
+  },
+  {
+    CourseId: "PROG200",
+    Title: "Introduction to JavaScript",
+    Location: "Classroom 9",
+    StartDate: "11/22/22",
+    Fee: "350.00",
+  },
+  {
+    CourseId: "PROG300",
+    Title: "Introduction to Java",
+    Location: "Classroom 1",
+    StartDate: "01/09/23",
+    Fee: "50.00",
+  },
+  {
+    CourseId: "PROG400",
+    Title: "Introduction to SQL and Databases",
+    Location: "Classroom 7",
+    StartDate: "03/16/23",
+    Fee: "50.00",
+  },
+  {
+    CourseId: "PROJ500",
+    Title: "Introduction to Angular",
+    Location: "Classroom 1",
+    StartDate: "04/25/23",
+    Fee: "50.00",
+  },
+];
+
+// When does the PROG200 course start?
+let searchCourseId = "PROG200";
+
+// look for courseid
+// and look for PROG200 value in it
+function searchCourses(course) {
+  return course.CourseId === searchCourseId;
+}
+
+// look through the courses
+let course = courses.find(searchCourses);
+
+// and look the start date in the course
+console.log(`${searchCourseId} starts on ${course.StartDate}`);
+
+
+
+
+// What is the title of the PROJ500 course?
+// What are the titles of the courses that cost $50 or less?
+// What classes meet in "Classroom 1"?
