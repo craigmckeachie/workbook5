07500dad86413eb3805644036c8cfd7159f42861
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-18 13:46:58 -0400

    mm candies filter

--- exercises/es6scripts/cheap_candy.js
+++ exercises/es6scripts/cheap_candy.js
@@ -1,6 +1,6 @@
 let products = [
   { product: "Gummy Worms", price: 5.79 },
-  { product: "Plain M&Ms", price: 2.89 },
+  { product: "Plain M&Ms gummy bears", price: 2.89 },
   { product: "Peanut M&Ms", price: 2.89 },
   { product: "Swedish Fish", price: 3.79 },
   { product: "Snickers", price: 2.79 },
@@ -9,6 +9,17 @@ let products = [
   { product: "Cow Tails", price: 1.67 },
 ];
 
-let cheaperProducts = products.filter((product) => product.price < 4.0);
+//cheap products
+// let cheaperProducts = products.filter((product) => product.price < 4.0);
+// console.log(cheaperProducts);
 
-console.log(cheaperProducts);
+let mmCandies1 = products.filter((product) => {
+  if (product.product.includes("M&Ms")) {
+    return true;
+  }
+  return false;
+});
+
+let mmCandies2 = products.filter((p) => p.product.includes("M&Ms"));
+console.log(mmCandies1);
+console.log(mmCandies2);
