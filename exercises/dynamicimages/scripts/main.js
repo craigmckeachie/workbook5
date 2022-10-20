b6d03a2326c719edadb8ca469b2858bfc66d9cb0
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-20 09:35:42 -0400

    find matching file working

--- exercises/dynamicimages/scripts/main.js
+++ exercises/dynamicimages/scripts/main.js
@@ -10,11 +10,29 @@ const guitarList = document.getElementById("guitarList");
 
 function loadGuitarList() {
   imageFiles.forEach((imageFile) => {
-    let option = new Option(imageFile.name, imageFile.path);
+    let option = new Option(imageFile.name, imageFile.name);
     guitarList.appendChild(option);
   });
 }
 
+function addImage() {
+  const selectedValue = guitarList.value;
+
+  //   const imagePath = null;
+  //   imageFiles.forEach((imageFile) => {
+  //     if (imageFile.name === selectedValue) {
+  //       imagePath = imageFile.path;
+  //       return;
+  //     }
+  //   });
+
+  let imageFile = imageFiles.find((f) => f.name === selectedValue);
+  console.log(imageFile.path);
+}
+
 window.onload = () => {
   loadGuitarList();
+
+  const addImageButton = document.getElementById("addImageButton");
+  addImageButton.onclick = addImage;
 };
